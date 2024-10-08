605d04c7511d78e472adc5953aa9c431772184d1
Klaas Nienhuis <mail@klaasnienhuis.nl>
2024-10-08 23:59:20 +0200

    model inspector

--- model-inspector/js/views/App.js
+++ model-inspector/js/views/App.js
@@ -1,42 +1,5 @@
 'use strict';
 
-var tplInfo = `
-<ul class="stat">
-    <li>
-        <span class="field">Vertices</span>
-        <span class="value"><%= info.vertexCount %></span>
-    </li>
-    <li>
-        <span class="field">Faces</span>
-        <span class="value"><%= info.faceCount %></span>
-    </li>
-    <li>
-        <span class="field">Renderer</span>
-        <span class="value"><%= info.options.shading.renderer %></span>
-    </li>
-    <li>
-        <span class="field">Materials</span>
-        <span class="value"><%= info.materialsCount %></span>
-    </li>
-</ul>
-`;
-var tplTextures = `
-<ul class="stat">
-    <li>
-        <span class="field">Textures</span>
-        <span class="value"><%= textures.count %></span>
-    </li>
-    <li>
-        <span class="field">Total Pixels</span>
-        <span class="value"><%= textures.pixelCount %></span>
-    </li>
-    <li>
-        <span class="field">VRAM</span>
-        <span class="value"><%= textures.vram %></span>
-    </li>
-</ul>
-`;
-
 var AppView = Backbone.View.extend({
     el: 'body',
 
@@ -50,14 +13,12 @@ var AppView = Backbone.View.extend({
         this._optionCollections = {};
         this._optionsViews = {};
         this._embedParams = {
-            continuousRender: 1,
-            preload: 1,
             ui_infos: 0,
             ui_stop: 0,
             debug3d: 1
         };
 
-        this.urlid = getParameterByName('urlid');
+        this.urlid = this.getParameterByName('urlid');
         if (!this.urlid) {
             return;
         }
@@ -70,6 +31,13 @@ var AppView = Backbone.View.extend({
         );
 
         this.hidden = [];
+    },
+
+    getParameterByName: function(name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+            results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     },
 
     loadModel: function() {
@@ -87,7 +55,7 @@ var AppView = Backbone.View.extend({
     initViewer: function(callback) {
         var self = this;
         var iframe = this.$el.find('#api-frame').get(0);
-        var version = '1.0.0';
+        var version = '1.12.1';
 
         var client = new Sketchfab(version, iframe);
 
@@ -107,9 +75,6 @@ var AppView = Backbone.View.extend({
                 }
             })
         );
-
-        this.getModelInfo();
-        this.getTextures();
     },
 
     toggle: function(e) {
@@ -157,6 +122,15 @@ var AppView = Backbone.View.extend({
                     return '<ul>' + nodes.join('') + '</ul>';
                 }
 
+                function escapeHtml(html) {
+                    return html
+                         .replace(/&/g, "&amp;")
+                         .replace(/</g, "&lt;")
+                         .replace(/>/g, "&gt;")
+                         .replace(/"/g, "&quot;")
+                         .replace(/'/g, "&#039;");
+                 }
+
                 function renderNode(node) {
                     var icons = {
                         Group: 'ion-folder',
@@ -171,7 +145,7 @@ var AppView = Backbone.View.extend({
                     out +=
                         '<i class="icon ' + icons[node.type] + '" title="' + node.type + '"></i> ';
 
-                    out += '<span>' + (node.name ? node.name : '(' + node.type + ')') + '</span>';
+                    out += '<span>' + (node.name ? escapeHtml(node.name) : '(' + node.type + ')') + '</span>';
 
                     if (node.children && node.children.length) {
                         out += renderChildren(node);
@@ -188,74 +162,10 @@ var AppView = Backbone.View.extend({
         );
     },
 
-    getTextures: function() {
-        var template = _.template(tplTextures);
-
-        $.ajax({
-            url: 'https://sketchfab.com/i/models/' + this.urlid + '/textures',
-            success: function(response) {
-                var textures = response.results;
-                for (var i = 0; i < textures.length; i++) {
-                    for (var j = 0; j < textures[i].images.length; j++) {
-                        if (textures[i].images[j].width === 32) {
-                            textures[i].preview = textures[i].images[j].url;
-                        }
-                    }
-                }
-
-                var textures = {};
-                textures.count = response.results.length;
-
-                var texture;
-                var textureSize;
-                var maxTextureSize;
-                var sumTextureSize = 0;
-                for (var i = 0; i < response.results.length; i++) {
-                    texture = response.results[i];
-
-                    maxTextureSize = 0;
-                    for (var j = 0; j < texture.images.length; j++) {
-                        textureSize = texture.images[j].width * texture.images[j].height;
-                        if (textureSize > maxTextureSize) {
-                            maxTextureSize = textureSize;
-                        }
-                        sumTextureSize += maxTextureSize;
-                    }
-                }
-                textures.pixelCount = sumTextureSize;
-                textures.vram = humanSize(sumTextureSize * 4);
-
-                $('.textures').html(
-                    template({
-                        textures: textures
-                    })
-                );
-            }
-        });
-    },
-
-    getModelInfo: function() {
-        var template = _.template(tplInfo);
-
-        $.ajax({
-            url: 'https://sketchfab.com/i/models/' + this.urlid,
-            success: function(response) {
-                var info = _.clone(response);
-                info.materialsCount = _.keys(info.options.materials).length;
-                $('.info').html(
-                    template({
-                        info: info
-                    })
-                );
-            }
-        });
-    },
-
     initPostProcessing: function() {
         var out = '<ul>';
 
         this._api.getPostProcessing(function(settings) {
-            // settings.enable
 
             if (settings.enable) {
                 $('input[name="postprocessing"]').prop('disabled', false);
